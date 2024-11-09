4f451a7ba8ceade0d4a16e64a19cc362ea54e016
Joseph Dada <josepholuwalajiki2004@gmail.com>
2024-11-09 18:19:53 +0100

    Modal video is now fixed

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const navbar = document.querySelector(".navbar");
   const closeButton = document.querySelector(".modal__close-button");
   const modal = document.getElementById("modal");
+  const modalVideo = document.getElementById("modal__video");
   const videoPlayerButton = document.querySelector(".preview__play-button");
 
   // Mobile Menu open / close
@@ -25,13 +26,14 @@ document.addEventListener("DOMContentLoaded", function () {
   // Open Modal section when the video-player button is clicked
   videoPlayerButton.addEventListener("click", function () {
     modal.style.display = "block";
-    modal.src = "https://www.youtube.com/embed/G0cmfY7qdmY?si=nvEFCm5TU8SrsQP-";
+    modalVideo.src =
+      "https://www.youtube.com/embed/G0cmfY7qdmY?si=nvEFCm5TU8SrsQP-";
   });
 
   // Close modal section on closeButton click
   closeButton.addEventListener("click", function () {
     modal.style.display = "none";
-    modal.src = "";
+    modalVideo.src = "";
   });
 
   modal.addEventListener("click", function () {
